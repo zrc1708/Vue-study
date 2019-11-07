bd07393093984792751d6e87aeafd2ca57fe707d
zrc1708 <1023054043@qq.com>
2019-11-07 21:53:51 +0800

    no message

---
+++ NewNode/sequelize-cli-demo/migrations/20191107111418-create-message.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Messages');
+  }
+};
---
+++ NewNode/sequelize-cli-demo/seeders/20191107123716-userTest.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('users',[
+     {
+       username:'nico',
+       age:16
+     },
+     {
+      username:'zhangsan',
+      age:18
+    }
+   ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+   return queryInterface.bulkDelete('users',null,{});
+  }
+};
