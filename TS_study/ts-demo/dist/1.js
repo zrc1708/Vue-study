8778a78d4048197cd9ac11463be99c0ead7af10d
zrc1708 <1023054043@qq.com>
2019-11-18 21:48:17 +0800

    no message

---
+++ TS_study/ts-demo/dist/1.js
@@ -0,0 +1,6 @@
+/**
+ * typeseript是JavaScript的超集
+ * typescript完全兼容JavaScript
+ */
+var str = 'hello ts';
+console.log(str);
---
+++ TS_study/ts-demo/src/1.ts
@@ -0,0 +1,11 @@
+/**
+ * typeseript是JavaScript的超集
+ * typescript完全兼容JavaScript
+ */
+
+import a from './2'
+
+ var str = 'hello ts'
+ console.log(str)
+ console.log(a)
+
