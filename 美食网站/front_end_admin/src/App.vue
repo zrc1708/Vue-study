6a428144e18e0b0c9a43cd87ecffb4cd45f918a6
zrc1708 <1023054043@qq.com>
2020-02-15 21:16:07 +0800

    no message

---
+++ 美食网站/front_end_admin/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <el-container>
+
+      <el-header>
+        <h1 class="h1 fl"><i class="iconfont icon-hotfood"></i>后台管理</h1>
+
+        <el-dropdown class="fr" trigger = "click">
+          <span class="el-dropdown-link">
+            Admin<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+
+      </el-header>
+
+      <el-container>
+        <el-aside>aside</el-aside>
+        <el-main>main</el-main>
+      </el-container>
+
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts">
+  import {
+    Vue,
+    Component
+  } from 'vue-property-decorator'
+
+  @Component
+  export default class App extends Vue {
+    name: string = '后台管理'
+  }
+</script>
