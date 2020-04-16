a15435da0e952cdc44296957ed6ce37d40dd5cd8
zrc1708 <1023054043@qq.com>
2020-04-16 21:53:51 +0800

    no message

--- 各种项目/企业官网/index.js
+++ 各种项目/企业官网/index.js
@@ -1,3 +1,31 @@
+const headerEl =document.querySelector('header')
+const scrollToTop = document.querySelector('.scrollToTop')
+
+window.addEventListener('scroll',()=>{
+    // 获取元素自身的高度
+    let height = headerEl.getBoundingClientRect().height
+    let glideitem = document.querySelector('.glide')
+    let aboutus = document.querySelector('.about-us')
+    // 顶部导航动画效果
+    if(window.pageYOffset-height>800){
+        if(!headerEl.classList.contains('sticky')){
+            headerEl.classList.add('sticky')
+            glideitem.style.top = '0'
+            aboutus.style.paddingTop = "80px"
+        }
+    }else{
+        headerEl.classList.remove('sticky')
+        glideitem.style.top = '-80px'
+        aboutus.style.paddingTop = "0"
+    }
+    // 返回顶部效果
+    if(window.pageYOffset>2000){
+        scrollToTop.style.display = 'block'
+    }else{
+        scrollToTop.style.display = 'none'
+    }
+})
+
 const glide = new Glide('.glide')
 const captionsEl = document.querySelectorAll('.slide__caption')
 
@@ -41,4 +69,70 @@ filterBtns.addEventListener('click', e =>{
 
         isotope.arrange({filter:filterOption})
     }
+})
+
+const staggeringOption ={
+    delay:300, //500毫秒出现
+    distance:"50px", //有50px的移动
+    duration:500,//动画执行500毫秒
+    easing:'ease-in-out',
+    origin:'bottom',//从下到上移动
+}
+// 案例，流程的出现动画效果
+// 第三方库scrollreveal相关配置,interval:每一个出现后等350出现下一个
+ScrollReveal().reveal('.feature',{...staggeringOption,interval:350})
+ScrollReveal().reveal('.service-item',{...staggeringOption,interval:350})
+// 数据的增长效果
+const dataSectionEl = document.querySelector('.data-section')
+ScrollReveal().reveal('.data-section',{
+    beforeReveal:()=>{
+        anime({
+            targets:'.num',
+            innerHTML:el=>{
+                return[0,el.innerHTML]
+            },
+            duration:2000,
+            round:1,//数字按1增长
+            easing:'easeInExpo',
+        })
+        // 更新一下背景的位置
+        dataSectionEl.style.backgroundPosition = 
+        `center calc(50% - ${dataSectionEl.getBoundingClientRect().bottom/5}px)`
+    }
+})
+
+// 背景视差效果
+window.addEventListener('scroll',()=>{
+    const bottom = dataSectionEl.getBoundingClientRect().bottom
+    const top = dataSectionEl.getBoundingClientRect().top
+    // 数据框出现在可是范围内
+    if(bottom>=0 && top<=window.innerHeight){
+        dataSectionEl.style.backgroundPosition = `center calc(50% - ${bottom /5}px)`
+    }
+})
+
+// 初始化滚动插件
+const scroll = new SmoothScroll('nav a[href*="#"], .scrollToTop a[href*="#"]',{
+    header:'header',//有固定导航
+    offset:80,//保持80px距离
+})
+
+// 展开导航时，选择后关闭导航
+document.addEventListener('scrollStart',()=>{
+    if(headerEl.classList.contains('open')){
+        headerEl.classList.remove('open')
+    }
+})
+
+const exploreBtnEls = document.querySelectorAll('.explors-btn')
+exploreBtnEls.forEach(item=>{
+    item.addEventListener('click',()=>{
+        scroll.animateScroll(document.querySelector('#about-us'))
+    })
+})
+
+// 折叠按钮
+const burgerEl = document.querySelector('.burger')
+burgerEl.addEventListener('click',()=>{
+    headerEl.classList.toggle('open')
 })
