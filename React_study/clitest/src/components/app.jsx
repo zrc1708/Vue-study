684d0d7821b36ec916b0b457918fb43e359b0e85
zrc1708 <1023054043@qq.com>
2020-05-24 22:25:59 +0800

    no message

---
+++ React_study/clitest/src/components/app.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class app extends Component {
+    render() {
+        return (
+            <div>
+                这是app根组件
+            </div>
+        )
+    }
+}
